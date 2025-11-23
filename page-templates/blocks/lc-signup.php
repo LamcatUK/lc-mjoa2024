b2628518b27b2a09fb5bf377f66b11d69fcfd361
LamcatUK <git@lamcat.co.uk>
2025-11-23 14:03:57 +0000

    signup

--- page-templates/blocks/lc-signup.php
+++ page-templates/blocks/lc-signup.php
@@ -10,9 +10,9 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <section class="signup bg--light py-5" data-aos="fade">
 	<div class="container-xl mx-4">
+		<h2 class="h2 headline mb-4">Join our Mailing List</h2>
 		<div class="row">
 			<div class="col-md-6">
-				<h2 class="h2 headline mb-4">Join our Mailing List</h2>
 				<div class="max-ch">
 					<p>Sign up to our mailing list to receive the latest news, upcoming hikes, and other exciting adventures.</p>
 					<p>You can unsubscribe anytime. For more details, review our Privacy Policy.</p>
@@ -20,9 +20,80 @@ defined( 'ABSPATH' ) || exit;
 			</div>
 			<div class="col-md-6">
 				<div class="signup__form">
-					<div class="ml-embedded" data-form="lzUim2"></div>
+					<!-- Begin MailerLite Signup Form -->
+<style type="text/css">
+.ml-form-embedSubmitLoad {
+	display: inline-block;
+	width: 20px;
+	height: 20px;
+}
+.ml-form-embedSubmitLoad:after {
+	content: " ";
+	display: block;
+	width: 11px;
+	height: 11px;
+	margin: 1px;
+	border-radius: 50%;
+	border: 4px solid #fff;
+	border-color: #6b3e0a #6b3e0a #6b3e0a transparent;
+	animation: ml-form-embedSubmitLoad 1.2s linear infinite;
+}
+@keyframes ml-form-embedSubmitLoad {
+	0% { transform: rotate(0deg); }
+	100% { transform: rotate(360deg); }
+}
+
+</style>
+      
+<div id="mlb2-22342525" class="ml-form-embedContainer ml-subscribe-form ml-subscribe-form-22342525">
+	<div class="ml-form-align-center ">
+        <div class="ml-form-embedWrapper embedForm">
+			<div class="ml-form-embedBody ml-form-embedBodyDefault row-form">
+				<form class="ml-block-form" action="https://assets.mailerlite.com/jsonp/1313077/forms/145436832930203348/subscribe" data-code="" method="post" target="_blank">
+					<div class="ml-form-formContent">
+						<div class="ml-form-fieldRow ml-last-item">
+							<div class="ml-field-group ml-field-email ml-validate-email ml-validate-required mb-4">
+								<!-- input -->
+								<input aria-label="email" aria-required="true" type="email" class="form-control" style="max-width:40vw" data-inputmask="" name="fields[email]" placeholder="Email" autocomplete="email">
+								<!-- /input -->
+							</div>
+						</div>
+					</div>              
+					<input type="hidden" name="ml-submit" value="1">
+					<div class="ml-form-embedSubmit">
+						<button type="submit" class="btn btn-primary primary">SUBSCRIBE</button>
+						<button disabled="disabled" style="display: none;" type="button" class="loading">
+							<div class="ml-form-embedSubmitLoad"></div>
+							<span class="sr-only">Loading...</span>
+						</button>
+					</div>
+					<input type="hidden" name="anticsrf" value="true">
+				</form>
+			</div>
+
+			<div class="ml-form-successBody row-success" style="display: none">
+				<div class="ml-form-successContent">
+					<h4>Thank you!</h4>
+					<p>You have successfully joined our subscriber list.</p>
 				</div>
 			</div>
-		</div>
+        </div>
 	</div>
+</div> 
+  <!-- End MailerLite Signup Form -->
+</div>
+</div>
+</div>
+</div>
 </section>
+<script>
+function ml_webform_success_22342525() {
+	var $ = ml_jQuery || jQuery;
+	$('.ml-subscribe-form-22342525 .row-success').show();
+	$('.ml-subscribe-form-22342525 .row-form').hide();
+}
+</script>
+<script src="https://groot.mailerlite.com/js/w/webforms.min.js?v176e10baa5e7ed80d35ae235be3d5024" type="text/javascript"></script>
+<script>
+fetch("https://assets.mailerlite.com/jsonp/1313077/forms/145436832930203348/takel")
+</script>
