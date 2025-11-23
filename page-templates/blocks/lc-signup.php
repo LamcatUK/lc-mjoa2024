92775d572ed9a6bc7a17720470a8676dc3876075
LamcatUK <git@lamcat.co.uk>
2025-11-23 13:49:30 +0000

    signup

--- page-templates/blocks/lc-signup.php
+++ page-templates/blocks/lc-signup.php
@@ -10,16 +10,17 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <section class="signup bg--light py-5" data-aos="fade">
 	<div class="container-xl mx-4">
-		<h2 class="h2 headline mb-4">Sign Up for Updates</h2>
 		<div class="row">
-			<div class="col-md-8">
-				<div class="max-ch mb-4">
-					Join our mailing list to receive the latest news, upcoming hikes, and other exciting adventures.
+			<div class="col-md-6">
+				<h2 class="h2 headline mb-4">Join our Mailing List</h2>
+				<div class="max-ch">
+					<p>Sign up to our mailing list to receive the latest news, upcoming hikes, and other exciting adventures.</p>
+					<p>You can unsubscribe anytime. For more details, review our Privacy Policy.</p>
 				</div>
 			</div>
-			<div class="col-md-4">
-				<div class="signup__form mb-4">
-					<button class="ml-onclick-form btn btn-primary" onclick="ml('show', 'lzUim2', true)">Join our mailing list</button>
+			<div class="col-md-6">
+				<div class="signup__form">
+					<div class="ml-embedded" data-form="lzUim2"></div>
 				</div>
 			</div>
 		</div>
